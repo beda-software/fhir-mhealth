9ca9459653e01878ff04113417eedf741ea48177
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-02 20:04:34 +0100

    Revert "Use yarn to run react-native bundler"
    This reverts commit 8d37a980456edcec8bca94e65edcebf179b6e5a1.

--- scripts/react-native-xcode.sh
+++ scripts/react-native-xcode.sh
@@ -10,21 +10,30 @@
 
 # Print commands before executing them (useful for troubleshooting)
 set -x
-set -e
 DEST=$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH
 
 # Path to react-native folder inside node_modules
-REACT_NATIVE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/node_modules/react-native/"
+REACT_NATIVE_DIR="./node_modules/react-native/"
 # The project should be located next to where react-native is installed
 # in node_modules.
 PROJECT_ROOT=${PROJECT_ROOT:-"$REACT_NATIVE_DIR/../.."}
 
+# check and assign NODE_BINARY env
+# shellcheck source=/dev/null
+source "$REACT_NATIVE_DIR/scripts/node-binary.sh"
+
 HERMES_ENGINE_PATH="$PODS_ROOT/hermes-engine"
-HERMES_CLI_PATH="$HERMES_ENGINE_PATH/destroot/bin/hermesc"
+
+[ -z "$HERMES_CLI_PATH" ] && HERMES_CLI_PATH="$HERMES_ENGINE_PATH/destroot/bin/hermesc"
+[ -z "$NODE_ARGS" ] && export NODE_ARGS=""
+[ -z "$CLI_PATH" ] && export CLI_PATH="$REACT_NATIVE_DIR/cli.js"
+[ -z "$BUNDLE_COMMAND" ] && BUNDLE_COMMAND="bundle"
 
 BUNDLE_FILE="$CONFIGURATION_BUILD_DIR/main.jsbundle"
 
-yarn run react-native bundle \
+echo "Running node $NODE_BINARY with bundle output $BUNDLE_FILE (assets dest $DEST)..." >&2
+
+"$NODE_BINARY" $NODE_ARGS "$CLI_PATH" $BUNDLE_COMMAND \
   --entry-file index.js \
   --platform ios \
   --dev false \
@@ -33,9 +42,15 @@ yarn run react-native bundle \
   --assets-dest "$DEST" \
   --minify false
  
+file $BUNDLE_FILE >&2
+
 "$HERMES_CLI_PATH" -emit-binary -O -out "$DEST/main.jsbundle" "$BUNDLE_FILE"
 
+file $BUNDLE_FILE >&2
+
 BUNDLE_FILE="$DEST/main.jsbundle"
+
+file $BUNDLE_FILE >&2
 
 if [[ ! -f "$BUNDLE_FILE" ]]; then
   echo "error: File $BUNDLE_FILE does not exist. This must be a bug with React Native, please report it here: https://github.com/facebook/react-native/issues" >&2
