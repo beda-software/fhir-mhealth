9ed5c504be583fa76d28fe6d99b6168c8e8e8c0e
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-02 17:05:10 +0100

    Revert "Use hermes-stable transformations with metro babel preset"
    This reverts commit 06c7f2213deb45bdce6a40f48172042a944c4693.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    presets: [
-        [
-            'module:metro-react-native-babel-preset',
-            {
-                unstable_transformProfile: 'hermes-stable',
-            },
-        ],
-    ],
+    presets: ['module:metro-react-native-babel-preset'],
 };
