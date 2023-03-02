339093959ede3a6b877798d0dde013aa7bf17e0d
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-02 20:00:23 +0100

    Revert "Add babel module-resolver plugin to map import paths"
    This reverts commit d2cb9f0923a2386c83dd0c9e85b5f4524159e02e.

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-        [
-            'module-resolver',
-            {
-                root: ['./src'],
-                extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-            },
-        ],
-    ],
 };
