3ef3facbd263675e89977e72b76e1d5d1862a25e
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-09 01:12:20 +0100

    Make mobx state tree persistent between app launches

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import { Notifications } from 'react-native-notifications';
 
 import { navigationLayout } from 'screens/ActivityFeed/navigation';
 import { launchBackgroundWorkoutRecordsSync } from 'services/datastream';
+import { restoreApplicationState } from 'models';
 
-Navigation.events().registerAppLaunchedListener(() => {
+Navigation.events().registerAppLaunchedListener(async () => {
+    await restoreApplicationState();
+
     Notifications.registerRemoteNotifications();
     Navigation.setRoot({
         root: {
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,9 @@
 import { createContext, useContext } from 'react';
-import { Instance, types } from 'mobx-state-tree';
+import { applySnapshot, Instance, onSnapshot, types } from 'mobx-state-tree';
+
+import { PersistentStorage } from 'services/storage';
+
+const PERSISTENCE_KEY = 'statetree.snapshot';
 
 const StateTreeModel = types.model('StateTreeModel').props({});
 
@@ -9,3 +13,12 @@ const root = StateTreeModel.create();
 const StateTreeContext = createContext<StateTree>(root);
 
 export const useStateTree = () => useContext<StateTree>(StateTreeContext);
+
+onSnapshot(root, (snapshot) => PersistentStorage.store(PERSISTENCE_KEY, snapshot));
+
+export async function restoreApplicationState() {
+    const latestSnapshot = await PersistentStorage.retrieve<StateTree>(PERSISTENCE_KEY).catch(() => undefined);
+    if (latestSnapshot !== undefined) {
+        applySnapshot(root, latestSnapshot);
+    }
+}
