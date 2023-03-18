49a6081861837c289bf47f141e6b36970837f740
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-18 15:34:09 +0100

    Extend app theme with colors palette

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,29 @@
 import { DynamicColorIOS, StyleSheet } from 'react-native';
 
-export const font = StyleSheet.create({
+export const palette = StyleSheet.create({
     primary: {
         color: DynamicColorIOS({
-            light: 'black',
+            light: '#3366FF',
+            dark: '#3366FF',
+        }),
+    },
+    secondary: {
+        color: DynamicColorIOS({
+            light: 'white',
             dark: 'white',
         }),
+    },
+    font: {
+        color: DynamicColorIOS({
+            light: 'white',
+            dark: 'white',
+        }),
+    },
+});
+
+export const font = StyleSheet.create({
+    primary: {
+        fontFamily: 'System',
+        color: palette.font.color,
     },
 });
