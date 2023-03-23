bd7d9e9153358d28cf7c514e3e51906054c9ca97
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-23 14:00:47 +0100

    Temporary disable ActivityFeed screen HealthKit subscription

--- src/screens/ActivityFeed/hooks.ts
+++ src/screens/ActivityFeed/hooks.ts
@@ -1,14 +1,7 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { Alert } from 'react-native';
 
-import {
-    HealthKitQueryController,
-    HealthKitQueryStatus,
-    useHealthKitQueryStatus,
-    useHealthKitWorkouts,
-} from 'services/healthkit';
-
-import { Activity, makeActivitiesCalendar } from './utils';
+import { Activity } from './utils';
 
 export type ActivityFeedItem = Activity;
 
@@ -18,37 +11,20 @@ export interface ActivityFeedSection {
 }
 
 export function useActivityFeed() {
-    const [workouts, setWorkouts] = useHealthKitWorkouts();
-    const feedStatus = useHealthKitQueryStatus();
-
     return {
-        activities: useMemo(
-            () =>
-                Array.from(makeActivitiesCalendar(workouts.slice().reverse())).reduce<ActivityFeedSection[]>(
-                    (sections, [date, oneDayActivities]) => {
-                        sections.push({ title: date, data: oneDayActivities });
-                        return sections;
-                    },
-                    [],
-                ),
-            [workouts],
-        ),
-        isRunning: feedStatus === HealthKitQueryStatus.Running ? true : false,
-        start: HealthKitQueryController.start,
-        stop: HealthKitQueryController.stop,
+        activities: [],
+        isRunning: true,
+        start: () => {},
+        stop: () => {},
         reset: useCallback(() => {
             Alert.alert('Reset history?', 'History reset will result in data duplicates', [
                 {
                     text: 'Reset',
                     style: 'destructive',
-                    onPress: () => {
-                        setWorkouts([]);
-                        HealthKitQueryController.reset();
-                        HealthKitQueryController.start();
-                    },
+                    onPress: () => {},
                 },
                 { text: 'Cancel', style: 'cancel' },
             ]);
-        }, [setWorkouts]),
+        }, []),
     };
 }
