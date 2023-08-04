ab6f1d4d5ee51ed901b254829de2e067b750077b
Dmitry Shutov <dmitry.s@beda.software>
2023-08-04 12:24:10 +0700

    Enable Metriport configuration in AppDelegate file

--- ios/FHIRmHealth/AppDelegate.mm
+++ ios/FHIRmHealth/AppDelegate.mm
@@ -11,6 +11,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [MetriportConfiguration checkBackgroundUpdates];
+  
   self.moduleName = @"FHIRmHealth";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
