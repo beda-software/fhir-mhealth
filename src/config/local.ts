75ded52da3e3c1ee916335e7a484e281849b2b7d
Dmitry Shutov <dmitry.s@beda.software>
2023-08-16 11:02:01 +0700

    Save Metriport Client key to config

--- src/config/local.ts
+++ src/config/local.ts
@@ -2,3 +2,4 @@ export const DATASTREAM_API_URL = 'http://localhost:8082/api/v1/records';
 export const DATASTREAM_METRIPORT_URL = 'http://localhost:8082/metriport';
 export const FHIR_API_URL = 'http://localhost:8080';
 export const SENTRY_DSN = undefined;
+export const METRIPORT_CLIENT_KEY = 'ODhVZ0JWcXlmakJYMDZqZWNwNHdYOmY5Y2RhYzA2LWIxMzItNGNmNC05MzIwLTAzMmE1MzVmZmY5MQ';
--- src/config/staging.ts
+++ src/config/staging.ts
@@ -2,3 +2,4 @@ export const DATASTREAM_API_URL = 'https://ingest.emr.beda.software/api/v1/recor
 export const DATASTREAM_METRIPORT_URL = 'https://ingest.emr.beda.software/metriport';
 export const FHIR_API_URL = 'https://aidbox.emr.beda.software';
 export const SENTRY_DSN = 'https://aaf5c7699f144d65bbb0c638a6ea7b67@o468421.ingest.sentry.io/4505090618294272';
+export const METRIPORT_CLIENT_KEY = 'ODhVZ0JWcXlmakJYMDZqZWNwNHdYOmY5Y2RhYzA2LWIxMzItNGNmNC05MzIwLTAzMmE1MzVmZmY5MQ';
